a9ba8ea536aaa823d5b38a9fea3b2b607cb1b629
Pavel Kuzin <pakuzin@edu.hse.ru>
2020-06-18 13:10:32 +0300

    Initial Commit

---
+++ JustTorch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustTorch
+//
+//  Created by Павел Кузин on 18.06.2020.
+//  Copyright © 2020 Павел Кузин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
