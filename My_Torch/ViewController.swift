9c329c68488f382c3499d6c7653a0d1f4bdf5c14
Pavel Kuzin <pakuzin@edu.hse.ru>
2020-06-18 13:16:30 +0300

    my first app ever

---
+++ My_Torch/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  JustTorch
+//
+//  Created by Павел Кузин on 10/05/2019.
+//  Copyright © 2019 Павел Кузин. All rights reserved.
+//
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+ {
+        super.viewDidLoad()
+ }
+
+    @IBAction func buttonPressed(_ sender: UIButton)
+    {
+        sender.contentEdgeInsets = UIEdgeInsets (top: 100, left: 100, bottom: 100, right: 100)
+
+        let device = AVCaptureDevice.default (for: AVMediaType.video)
+
+// Checking the devise has Torch or not
+        if device?.hasTorch ?? true
+        {
+            do {
+                try
+                    device?.lockForConfiguration()
+                    device?.torchMode = device?.torchMode == AVCaptureDevice.TorchMode.on ? .off : .on
+
+                switch device?.torchMode
+                {
+
+// Changing the button title from "Torch" to "Torch On" and "Torch OFF" if the Torch is Lighting
+                    case AVCaptureDevice.TorchMode.on?:
+                        sender.setTitle("Torch ON", for: UIControl.State.normal)
+                    case AVCaptureDevice.TorchMode.off?:
+                        sender.setTitle("Torch OFF", for: UIControl.State.normal)
+                    default:
+                        print("Error")
+                }
+                    device?.unlockForConfiguration()
+                } catch{
+                print(error)
+                }
+        }
+// If the device hasn't a Torch button title will change to "Sorry, but your device doesn't has a torch"
+        else {sender.setTitle("Sorry, but your device doesn't has a torch", for: UIControl.State.normal)}
+    }
+}
